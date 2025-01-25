c6bd2f7dec7e9adabb543e60ccfd7085e2e8a136
Ryvqi <rifqikhabibi03@gmail.com>
2025-01-25 17:47:14 +0700

    menambahkan data class Properti

--- app/src/main/java/com/example/bisnisproperti/model/Properti.kt
+++ app/src/main/java/com/example/bisnisproperti/model/Properti.kt
@@ -1,2 +1,44 @@
 package com.example.bisnisproperti.model
 
+import kotlinx.serialization.*
+
+@Serializable
+data class Properti(
+    @SerialName("id_properti")
+    val idProperti: Int,
+
+    @SerialName("nama_properti")
+    val namaProperti: String,
+
+    @SerialName("deskripsi_properti")
+    val deskripsiProperti: String,
+
+    val lokasi: String,
+    val harga: String,
+
+    @SerialName("status_properti")
+    val statusProperti: String,
+
+    @SerialName("id_jenis")
+    val idJenis: Int,
+
+    @SerialName("id_pemilik")
+    val idPemilik: Int,
+
+    @SerialName("id_manajer")
+    val idManajer: Int
+)
+
+@Serializable
+data class AllPropertiResponse(
+    val status: Boolean,
+    val message: String,
+    val data: List<Properti>
+)
+
+@Serializable
+data class PropertiDetailResponse(
+    val status: Boolean,
+    val message: String,
+    val data: Properti
+)
