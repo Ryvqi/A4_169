48d070773b35a0052d3df0bcc3e73000a7b1f510
Ryvqi <rifqikhabibi03@gmail.com>
2025-01-25 17:46:41 +0700

    menambahkan data class Pemilik

--- app/src/main/java/com/example/bisnisproperti/model/Pemilik.kt
+++ app/src/main/java/com/example/bisnisproperti/model/Pemilik.kt
@@ -1,2 +1,29 @@
 package com.example.bisnisproperti.model
 
+import kotlinx.serialization.*
+
+@Serializable
+data class Pemilik(
+    @SerialName("id_pemilik")
+    val idPemilik: Int,
+
+    @SerialName("nama_pemilik")
+    val namaPemilik: String,
+
+    @SerialName("kontak_pemilik")
+    val kontakPemilik: String
+)
+
+@Serializable
+data class AllPemilikResponse(
+    val status: Boolean,
+    val message: String,
+    val data: List<Pemilik>
+)
+
+@Serializable
+data class PemilikDetailResponse(
+    val status: Boolean,
+    val message: String,
+    val data: Pemilik
+)
