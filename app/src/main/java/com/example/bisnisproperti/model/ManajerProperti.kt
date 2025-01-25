80fc98fbe232097f5d00b5e37e03aa7aca3f9f51
Ryvqi <rifqikhabibi03@gmail.com>
2025-01-25 17:46:01 +0700

    menambahkan data class ManajerProperti

--- app/src/main/java/com/example/bisnisproperti/model/ManajerProperti.kt
+++ app/src/main/java/com/example/bisnisproperti/model/ManajerProperti.kt
@@ -1,2 +1,29 @@
 package com.example.bisnisproperti.model
 
+import kotlinx.serialization.*
+
+@Serializable
+data class ManajerProperti(
+    @SerialName("id_manajer")
+    val idManajer: Int,
+
+    @SerialName("nama_manajer")
+    val namaManajer: String,
+
+    @SerialName("kontak_manajer")
+    val kontakManajer: String
+)
+
+@Serializable
+data class AllManajerPropertiResponse(
+    val status: Boolean,
+    val message: String,
+    val data: List<ManajerProperti>
+)
+
+@Serializable
+data class ManajerPropertiDetailResponse(
+    val status: Boolean,
+    val message: String,
+    val data: ManajerProperti
+)
