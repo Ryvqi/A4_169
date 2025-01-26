77f8d24bfcf3fb9d0a3f0f1f9ce16d42acce200c
Ryvqi <rifqikhabibi03@gmail.com>
2025-01-26 08:15:14 +0700

    menambahkan interface PropertiRepository

--- app/src/main/java/com/example/bisnisproperti/repository/PropertiRepo.kt
+++ app/src/main/java/com/example/bisnisproperti/repository/PropertiRepo.kt
@@ -1,2 +1,13 @@
 package com.example.bisnisproperti.repository
 
+import com.example.bisnisproperti.model.AllPropertiResponse
+import com.example.bisnisproperti.model.Properti
+
+interface PropertiRepository {
+    suspend fun getAllProperti(): AllPropertiResponse
+    suspend fun getPropertiById(id: String): Properti
+    suspend fun insertProperti(properti: Properti)
+    suspend fun updateProperti(id: String, properti: Properti)
+    suspend fun deleteProperti(id: String): String
+}
+
