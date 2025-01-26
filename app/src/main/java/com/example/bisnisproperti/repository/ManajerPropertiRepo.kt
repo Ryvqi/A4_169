b3061f49a8379c0e9e71a340a4203d4051170970
Ryvqi <rifqikhabibi03@gmail.com>
2025-01-26 08:19:08 +0700

    menambahkan class NetworkManajerPropertiRepository

--- app/src/main/java/com/example/bisnisproperti/repository/ManajerPropertiRepo.kt
+++ app/src/main/java/com/example/bisnisproperti/repository/ManajerPropertiRepo.kt
@@ -2,6 +2,7 @@ package com.example.bisnisproperti.repository
 
 import com.example.bisnisproperti.model.AllManajerPropertiResponse
 import com.example.bisnisproperti.model.ManajerProperti
+import com.example.bisnisproperti.service.ManajerPropertiService
 
 interface ManajerPropertiRepository {
     suspend fun getAllManajerProperti(): AllManajerPropertiResponse
@@ -11,3 +12,25 @@ interface ManajerPropertiRepository {
     suspend fun deleteManajerProperti(id: String): String
 }
 
+class NetworkManajerPropertiRepository(
+    private val manajerPropertiService: ManajerPropertiService
+) : ManajerPropertiRepository {
+    override suspend fun getAllManajerProperti() = manajerPropertiService.getAllManajerProperti()
+
+    override suspend fun getManajerPropertiById(id: String) = manajerPropertiService.getManajerPropertiById(id)
+
+    override suspend fun insertManajerProperti(manajerProperti: ManajerProperti) =
+        manajerPropertiService.insertManajerProperti(manajerProperti)
+
+    override suspend fun updateManajerProperti(id: String, manajerProperti: ManajerProperti) =
+        manajerPropertiService.updateManajerProperti(id, manajerProperti)
+
+    override suspend fun deleteManajerProperti(id: String): String {
+        val response = manajerPropertiService.deleteManajerProperti(id)
+        return if (response.isSuccessful) {
+            "Manajer Properti berhasil dihapus"
+        } else {
+            "Gagal menghapus manajer properti: ${response.errorBody()?.string() ?: "Unknown error"}"
+        }
+    }
+}
