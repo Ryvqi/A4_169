f4ab43a49acf6d359b8c26cc56fa1001243fd476
Ryvqi <rifqikhabibi03@gmail.com>
2025-01-26 08:16:42 +0700

    menambahkan interface JenisPropertiRepository

--- app/src/main/java/com/example/bisnisproperti/repository/JenisPropertiRepo.kt
+++ app/src/main/java/com/example/bisnisproperti/repository/JenisPropertiRepo.kt
@@ -1,2 +1,13 @@
 package com.example.bisnisproperti.repository
 
+import com.example.bisnisproperti.model.AllJenisPropertiResponse
+import com.example.bisnisproperti.model.JenisProperti
+
+interface JenisPropertiRepository {
+    suspend fun getAllJenisProperti(): AllJenisPropertiResponse
+    suspend fun getJenisPropertiById(id: String): JenisProperti
+    suspend fun insertJenisProperti(jenisProperti: JenisProperti)
+    suspend fun updateJenisProperti(id: String, jenisProperti: JenisProperti)
+    suspend fun deleteJenisProperti(id: String): String
+}
+
