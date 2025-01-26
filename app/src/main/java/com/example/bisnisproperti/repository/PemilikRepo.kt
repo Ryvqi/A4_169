5f09f8049cad40caacd5fa898cd54af66c14ce9c
Ryvqi <rifqikhabibi03@gmail.com>
2025-01-26 08:17:50 +0700

    menambahkan interface PemilikRepository

--- app/src/main/java/com/example/bisnisproperti/repository/PemilikRepo.kt
+++ app/src/main/java/com/example/bisnisproperti/repository/PemilikRepo.kt
@@ -1,2 +1,13 @@
 package com.example.bisnisproperti.repository
 
+import com.example.bisnisproperti.model.AllPemilikResponse
+import com.example.bisnisproperti.model.Pemilik
+
+interface PemilikRepository {
+    suspend fun getAllPemilik(): AllPemilikResponse
+    suspend fun getPemilikById(id: String): Pemilik
+    suspend fun insertPemilik(pemilik: Pemilik)
+    suspend fun updatePemilik(id: String, pemilik: Pemilik)
+    suspend fun deletePemilik(id: String): String
+}
+
