d1f8009f1fb0afb5cdb95f0cde49846adcab4871
Ryvqi <rifqikhabibi03@gmail.com>
2025-01-26 08:17:18 +0700

    menambahkan class NetworkJenisPropertiRepository

--- app/src/main/java/com/example/bisnisproperti/repository/JenisPropertiRepo.kt
+++ app/src/main/java/com/example/bisnisproperti/repository/JenisPropertiRepo.kt
@@ -2,6 +2,7 @@ package com.example.bisnisproperti.repository
 
 import com.example.bisnisproperti.model.AllJenisPropertiResponse
 import com.example.bisnisproperti.model.JenisProperti
+import com.example.bisnisproperti.service.JenisPropertiService
 
 interface JenisPropertiRepository {
     suspend fun getAllJenisProperti(): AllJenisPropertiResponse
@@ -11,3 +12,25 @@ interface JenisPropertiRepository {
     suspend fun deleteJenisProperti(id: String): String
 }
 
+class NetworkJenisPropertiRepository(
+    private val jenisPropertiService: JenisPropertiService
+) : JenisPropertiRepository {
+    override suspend fun getAllJenisProperti() = jenisPropertiService.getAllJenisProperti()
+
+    override suspend fun getJenisPropertiById(id: String) = jenisPropertiService.getJenisPropertiById(id)
+
+    override suspend fun insertJenisProperti(jenisProperti: JenisProperti) =
+        jenisPropertiService.insertJenisProperti(jenisProperti)
+
+    override suspend fun updateJenisProperti(id: String, jenisProperti: JenisProperti) =
+        jenisPropertiService.updateJenisProperti(id, jenisProperti)
+
+    override suspend fun deleteJenisProperti(id: String): String {
+        val response = jenisPropertiService.deleteJenisProperti(id)
+        return if (response.isSuccessful) {
+            "Jenis Properti berhasil dihapus"
+        } else {
+            "Gagal menghapus jenis properti: ${response.errorBody()?.string() ?: "Unknown error"}"
+        }
+    }
+}
