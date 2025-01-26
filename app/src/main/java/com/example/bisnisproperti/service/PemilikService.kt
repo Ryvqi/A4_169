606102154e181c7c711b753fc8f940774186f764
Ryvqi <rifqikhabibi03@gmail.com>
2025-01-26 08:12:21 +0700

    menambahkan service untuk Pemilik

--- app/src/main/java/com/example/bisnisproperti/service/PemilikService.kt
+++ app/src/main/java/com/example/bisnisproperti/service/PemilikService.kt
@@ -1,4 +1,29 @@
 package com.example.bisnisproperti.service
 
+import com.example.bisnisproperti.model.AllPemilikResponse
+import com.example.bisnisproperti.model.Pemilik
+import retrofit2.Response
+import retrofit2.http.*
+
 interface PemilikService {
+
+    @Headers(
+        "Accept: application/json",
+        "Content-Type: application/json"
+    )
+
+    @GET("pemilik")
+    suspend fun getAllPemilik(): AllPemilikResponse
+
+    @GET("pemilik/{id}")
+    suspend fun getPemilikById(@Path("id") id: String): Pemilik
+
+    @POST("pemilik")
+    suspend fun insertPemilik(@Body pemilik: Pemilik)
+
+    @PUT("pemilik/{id}")
+    suspend fun updatePemilik(@Path("id") id: String, @Body pemilik: Pemilik)
+
+    @DELETE("pemilik/{id}")
+    suspend fun deletePemilik(@Path("id") id: String): Response<Void>
 }
