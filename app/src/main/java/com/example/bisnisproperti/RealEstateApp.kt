bed8db1eda2441c610e7779d7e622dad415b1b04
Ryvqi <rifqikhabibi03@gmail.com>
2025-01-26 08:26:57 +0700

    Setup dependency injection container untuk RealEstateApp

--- app/src/main/java/com/example/bisnisproperti/RealEstateApp.kt
+++ app/src/main/java/com/example/bisnisproperti/RealEstateApp.kt
@@ -1,5 +1,13 @@
 package com.example.bisnisproperti
 
 import android.app.Application
+import com.example.bisnisproperti.dependesiinjeksi.AppContainer
+import com.example.bisnisproperti.dependesiinjeksi.PropertiContainer
 
-class RealEstateApp: Application(){}
+class RealEstateApp: Application(){
+    lateinit var container: AppContainer
+    override fun onCreate() {
+        super.onCreate()
+        container = PropertiContainer()
+    }
+}
