e798d79ea8d04941c192b800bbd64a3889280f2d
Ryvqi <rifqikhabibi03@gmail.com>
2025-01-26 08:22:03 +0700

    menambahkan properti untuk setiap container

--- app/src/main/java/com/example/bisnisproperti/dependesiinjeksi/ContainerApp.kt
+++ app/src/main/java/com/example/bisnisproperti/dependesiinjeksi/ContainerApp.kt
@@ -1,2 +1,14 @@
 package com.example.bisnisproperti.dependesiinjeksi
 
+import com.example.bisnisproperti.repository.JenisPropertiRepository
+import com.example.bisnisproperti.repository.ManajerPropertiRepository
+import com.example.bisnisproperti.repository.PemilikRepository
+import com.example.bisnisproperti.repository.PropertiRepository
+
+interface AppContainer {
+    val propertiRepository: PropertiRepository
+    val jenisPropertiRepository: JenisPropertiRepository
+    val pemilikRepository: PemilikRepository
+    val manajerPropertiRepository: ManajerPropertiRepository
+}
+
