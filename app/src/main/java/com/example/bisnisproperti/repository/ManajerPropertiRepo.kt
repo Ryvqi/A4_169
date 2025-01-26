741c53305c8c1d964b3fd7201199bf7df422221b
Ryvqi <rifqikhabibi03@gmail.com>
2025-01-26 08:18:39 +0700

    menambahkan interface ManajerPropertiRepository

--- app/src/main/java/com/example/bisnisproperti/repository/ManajerPropertiRepo.kt
+++ app/src/main/java/com/example/bisnisproperti/repository/ManajerPropertiRepo.kt
@@ -1,2 +1,13 @@
 package com.example.bisnisproperti.repository
 
+import com.example.bisnisproperti.model.AllManajerPropertiResponse
+import com.example.bisnisproperti.model.ManajerProperti
+
+interface ManajerPropertiRepository {
+    suspend fun getAllManajerProperti(): AllManajerPropertiResponse
+    suspend fun getManajerPropertiById(id: String): ManajerProperti
+    suspend fun insertManajerProperti(manajerProperti: ManajerProperti)
+    suspend fun updateManajerProperti(id: String, manajerProperti: ManajerProperti)
+    suspend fun deleteManajerProperti(id: String): String
+}
+
