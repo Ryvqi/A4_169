1dcbdad07c9166d341edcce7b967a1ec7c4f6060
Ryvqi <rifqikhabibi03@gmail.com>
2025-01-28 00:58:12 +0700

    menambahkan fungsi updateproperti

--- app/src/main/java/com/example/bisnisproperti/ui/viewmodel/properti/UpdatePropertiViewModel.kt
+++ app/src/main/java/com/example/bisnisproperti/ui/viewmodel/properti/UpdatePropertiViewModel.kt
@@ -5,8 +5,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.bisnisproperti.model.Properti
 import com.example.bisnisproperti.repository.PropertiRepository
+import kotlinx.coroutines.launch
 
 class UpdatePropertiViewModel(
     savedStateHandle: SavedStateHandle,
@@ -16,6 +18,24 @@ class UpdatePropertiViewModel(
         private set
 
     private val idProperti: Int = checkNotNull(savedStateHandle["id_properti"])
+
+    init {
+        viewModelScope.launch {
+            val properti = propertiRepository.getPropertiById(idProperti.toString())
+            uiState = properti.toInsertpropertiUiEvent()
+        }
+    }
+
+    fun updateProperti() {
+        viewModelScope.launch {
+            try {
+                val properti = uiState.insertPropertiUiEvent.toProperti()
+                propertiRepository.updateProperti(idProperti.toString(), properti)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
 }
 
 fun Properti.toInsertpropertiUiEvent(): InsertPropertiUiState = InsertPropertiUiState(
