f03dea01935bf76006c5c4bb1d35d766a5ea485e
Ryvqi <rifqikhabibi03@gmail.com>
2025-01-28 00:55:12 +0700

    menambahkan class UpdatePropertiViewModel

--- app/src/main/java/com/example/bisnisproperti/ui/viewmodel/properti/UpdatePropertiViewModel.kt
+++ app/src/main/java/com/example/bisnisproperti/ui/viewmodel/properti/UpdatePropertiViewModel.kt
@@ -1,2 +1,18 @@
 package com.example.bisnisproperti.ui.viewmodel.properti
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.bisnisproperti.repository.PropertiRepository
+
+class UpdatePropertiViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val propertiRepository: PropertiRepository
+) : ViewModel() {
+    var uiState by mutableStateOf(InsertPropertiUiState())
+        private set
+
+    private val idProperti: Int = checkNotNull(savedStateHandle["id_properti"])
+}
