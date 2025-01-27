d1efc555caec21c55d75603543d8f83c19f06f7b
Ryvqi <rifqikhabibi03@gmail.com>
2025-01-27 23:02:16 +0700

    Menerapkan fungsi insertProperti dengan validasi input

--- app/src/main/java/com/example/bisnisproperti/ui/viewmodel/properti/InsertPropertiViewModel.kt
+++ app/src/main/java/com/example/bisnisproperti/ui/viewmodel/properti/InsertPropertiViewModel.kt
@@ -4,14 +4,40 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.bisnisproperti.model.Properti
 import com.example.bisnisproperti.repository.PropertiRepository
+import kotlinx.coroutines.launch
 
 class InsertPropertiViewModel(private val propertiRepository: PropertiRepository) : ViewModel() {
     var uiState by mutableStateOf(InsertPropertiUiState())
         private set
 
+    fun insertProperti() {
+        viewModelScope.launch {
+            try {
+                // Validasi input
+                if (validateInput(uiState.insertPropertiUiEvent)) {
+                    propertiRepository.insertProperti(uiState.insertPropertiUiEvent.toProperti())
+                } else {
+                    throw IllegalArgumentException("Input tidak valid")
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
 
+    private fun validateInput(event: InsertPropertiUiEvent): Boolean {
+        return event.namaProperti.isNotBlank() &&
+                event.deskripsiProperti.isNotBlank() &&
+                event.lokasi.isNotBlank() &&
+                event.harga.isNotBlank() &&
+                event.statusProperti.isNotBlank() &&
+                event.idJenis > 0 &&
+                event.idPemilik > 0 &&
+                event.idManajer > 0
+    }
 }
 
 data class InsertPropertiUiEvent(
