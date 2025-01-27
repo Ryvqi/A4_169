ec1be48197015f07cea2d547ff88518fc668ab08
Ryvqi <rifqikhabibi03@gmail.com>
2025-01-27 23:49:44 +0700

    menambahkan sealed class DetailPropertiUiState

--- app/src/main/java/com/example/bisnisproperti/ui/viewmodel/properti/DetailPropertiViewModel.kt
+++ app/src/main/java/com/example/bisnisproperti/ui/viewmodel/properti/DetailPropertiViewModel.kt
@@ -1,2 +1,12 @@
 package com.example.bisnisproperti.ui.viewmodel.properti
 
+import com.example.bisnisproperti.model.Properti
+
+sealed class DetailPropertiUiState {
+    data class Success(
+        val properti: Properti
+    ) : DetailPropertiUiState()
+
+    object Error : DetailPropertiUiState()
+    object Loading : DetailPropertiUiState()
+}
