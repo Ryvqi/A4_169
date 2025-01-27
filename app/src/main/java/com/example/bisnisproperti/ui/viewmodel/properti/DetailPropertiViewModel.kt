9ea370daf7c7d747ebff0b03e4bfdd491557c8a7
Ryvqi <rifqikhabibi03@gmail.com>
2025-01-27 23:51:29 +0700

    menambahkan class DetailPropertiViewModel

--- app/src/main/java/com/example/bisnisproperti/ui/viewmodel/properti/DetailPropertiViewModel.kt
+++ app/src/main/java/com/example/bisnisproperti/ui/viewmodel/properti/DetailPropertiViewModel.kt
@@ -1,6 +1,12 @@
 package com.example.bisnisproperti.ui.viewmodel.properti
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
 import com.example.bisnisproperti.model.Properti
+import com.example.bisnisproperti.repository.PropertiRepository
 
 sealed class DetailPropertiUiState {
     data class Success(
@@ -9,4 +15,14 @@ sealed class DetailPropertiUiState {
 
     object Error : DetailPropertiUiState()
     object Loading : DetailPropertiUiState()
+}
+
+class DetailPropertiViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val propertiRepository: PropertiRepository
+) : ViewModel() {
+    var propertiUiState: DetailPropertiUiState by mutableStateOf(DetailPropertiUiState.Loading)
+        private set
+
+    private val idProperti: Int = checkNotNull(savedStateHandle["id_properti"])
 }
