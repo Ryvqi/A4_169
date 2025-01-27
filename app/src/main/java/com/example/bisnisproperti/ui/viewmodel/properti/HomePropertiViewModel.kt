3d88e11368741e9470f09160bccc9c40279e2e93
Ryvqi <rifqikhabibi03@gmail.com>
2025-01-27 23:40:24 +0700

    menambahkan HomePropertiViewModel

--- app/src/main/java/com/example/bisnisproperti/ui/viewmodel/properti/HomePropertiViewModel.kt
+++ app/src/main/java/com/example/bisnisproperti/ui/viewmodel/properti/HomePropertiViewModel.kt
@@ -1,6 +1,11 @@
 package com.example.bisnisproperti.ui.viewmodel.properti
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
 import com.example.bisnisproperti.model.Properti
+import com.example.bisnisproperti.repository.PropertiRepository
 
 sealed class HomePropertiUiState {
     data class Success(
@@ -9,4 +14,9 @@ sealed class HomePropertiUiState {
 
     object Error : HomePropertiUiState()
     object Loading : HomePropertiUiState()
+}
+
+class HomePropertiViewModel(private val propertiRepository: PropertiRepository) : ViewModel() {
+    var propertiUiState: HomePropertiUiState by mutableStateOf(HomePropertiUiState.Loading)
+        private set
 }
