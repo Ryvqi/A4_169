3e84c84a29b72fb26b393dcfbf1db36ccb8f1483
Ryvqi <rifqikhabibi03@gmail.com>
2025-01-27 23:51:59 +0700

    menambahkan fungsi getpropertibyid

--- app/src/main/java/com/example/bisnisproperti/ui/viewmodel/properti/DetailPropertiViewModel.kt
+++ app/src/main/java/com/example/bisnisproperti/ui/viewmodel/properti/DetailPropertiViewModel.kt
@@ -5,8 +5,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.bisnisproperti.model.Properti
 import com.example.bisnisproperti.repository.PropertiRepository
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
 
 sealed class DetailPropertiUiState {
     data class Success(
@@ -25,4 +29,21 @@ class DetailPropertiViewModel(
         private set
 
     private val idProperti: Int = checkNotNull(savedStateHandle["id_properti"])
+
+    init {
+        getPropertiById()
+    }
+
+    fun getPropertiById() {
+        viewModelScope.launch {
+            propertiUiState = DetailPropertiUiState.Loading
+            propertiUiState = try {
+                DetailPropertiUiState.Success(propertiRepository.getPropertiById(idProperti.toString()))
+            } catch (e: IOException) {
+                DetailPropertiUiState.Error
+            } catch (e: HttpException) {
+                DetailPropertiUiState.Error
+            }
+        }
+    }
 }
