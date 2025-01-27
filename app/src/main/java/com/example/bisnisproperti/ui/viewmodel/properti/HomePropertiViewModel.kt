e44f6fcc9de9870b69e3c5a2a0dcc04d31192c36
Ryvqi <rifqikhabibi03@gmail.com>
2025-01-27 23:37:45 +0700

    menambahkan sealed class HomePropertiUiState

--- app/src/main/java/com/example/bisnisproperti/ui/viewmodel/properti/HomePropertiViewModel.kt
+++ app/src/main/java/com/example/bisnisproperti/ui/viewmodel/properti/HomePropertiViewModel.kt
@@ -1,2 +1,12 @@
 package com.example.bisnisproperti.ui.viewmodel.properti
 
+import com.example.bisnisproperti.model.Properti
+
+sealed class HomePropertiUiState {
+    data class Success(
+        val properti: List<Properti>
+    ) : HomePropertiUiState()
+
+    object Error : HomePropertiUiState()
+    object Loading : HomePropertiUiState()
+}
