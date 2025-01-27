06d48f429232e2cda027e42138ee70ca8e4fc476
Ryvqi <rifqikhabibi03@gmail.com>
2025-01-27 20:42:51 +0700

    menyesuaikan endpoint JenisPropertiService dengan backend

--- app/src/main/java/com/example/bisnisproperti/service/JenisPropertiService.kt
+++ app/src/main/java/com/example/bisnisproperti/service/JenisPropertiService.kt
@@ -15,15 +15,18 @@ interface JenisPropertiService {
     @GET("jenis_properti")
     suspend fun getAllJenisProperti(): AllJenisPropertiResponse
 
-    @GET("jenis_properti/{id}")
-    suspend fun getJenisPropertiById(@Path("id") id: String): JenisProperti
+    @GET("jenis_properti/{id_jenis}")
+    suspend fun getJenisPropertiById(@Path("id_jenis") idJenis: String): JenisProperti
 
-    @POST("jenis_properti")
-    suspend fun insertJenisProperti(@Body jenisProperti: JenisProperti)
+    @POST("jenis_properti/store")
+    suspend fun insertJenisProperti(@Body jenisProperti: JenisProperti): Response<JenisProperti>
 
-    @PUT("jenis_properti/{id}")
-    suspend fun updateJenisProperti(@Path("id") id: String, @Body jenisProperti: JenisProperti)
+    @PUT("jenis_properti/{id_jenis}")
+    suspend fun updateJenisProperti(
+        @Path("id_jenis") idJenis: String,
+        @Body jenisProperti: JenisProperti
+    ): Response<JenisProperti>
 
-    @DELETE("jenis_properti/{id}")
-    suspend fun deleteJenisProperti(@Path("id") id: String): Response<Void>
+    @DELETE("jenis_properti/{id_jenis}")
+    suspend fun deleteJenisProperti(@Path("id_jenis") idJenis: String): Response<Void>
 }
