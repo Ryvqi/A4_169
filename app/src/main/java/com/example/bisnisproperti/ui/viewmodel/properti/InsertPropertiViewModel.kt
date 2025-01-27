3083b15b14d8cffd264863dba606c49560d123a1
Ryvqi <rifqikhabibi03@gmail.com>
2025-01-27 22:51:26 +0700

    Menambah data class InsertPropertiUiEvent

--- app/src/main/java/com/example/bisnisproperti/ui/viewmodel/properti/InsertPropertiViewModel.kt
+++ app/src/main/java/com/example/bisnisproperti/ui/viewmodel/properti/InsertPropertiViewModel.kt
@@ -1,2 +1,13 @@
 package com.example.bisnisproperti.ui.viewmodel.properti
 
+data class InsertPropertiUiEvent(
+    val idProperti: Int = 0,
+    val namaProperti: String = "",
+    val deskripsiProperti: String = "",
+    val lokasi: String = "",
+    val harga: String = "",
+    val statusProperti: String = "",
+    val idJenis: Int = 0,
+    val idPemilik: Int = 0,
+    val idManajer: Int = 0
+)
