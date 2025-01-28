8aaaaf8ba9231e8d9df42152f25c98f04f8fff8a
Ryvqi <rifqikhabibi03@gmail.com>
2025-01-28 10:25:17 +0700

    menambahkan fungsi DetailRow

--- app/src/main/java/com/example/bisnisproperti/ui/view/properti/DetailPropertiView.kt
+++ app/src/main/java/com/example/bisnisproperti/ui/view/properti/DetailPropertiView.kt
@@ -1,2 +1,26 @@
 package com.example.bisnisproperti.ui.view.properti
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun DetailRow(
+    modifier: Modifier = Modifier,
+    judul: String,
+    isi: String
+) {
+    Column(
+        modifier = modifier.fillMaxWidth()
+    ) {
+        Text(
+            text = "$judul:",
+            style = MaterialTheme.typography.titleMedium
+        )
+        Text(
+            text = isi,
+            style = MaterialTheme.typography.bodyMedium
+        )
+    }
+}
