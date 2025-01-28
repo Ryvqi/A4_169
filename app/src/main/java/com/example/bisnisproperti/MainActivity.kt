7c4715062efee25dbb6cd71739081b93832154b5
Ryvqi <rifqikhabibi03@gmail.com>
2025-01-28 23:16:11 +0700

    refactor greeting to pengelolahalaman

--- app/src/main/java/com/example/bisnisproperti/MainActivity.kt
+++ app/src/main/java/com/example/bisnisproperti/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.bisnisproperti.navigasi.PengelolaHalaman
 import com.example.bisnisproperti.ui.theme.BisnisPropertiTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             BisnisPropertiTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    PengelolaHalaman(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
