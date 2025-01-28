2537834242a7014954b7fc49f5668222244b0588
Ryvqi <rifqikhabibi03@gmail.com>
2025-01-28 16:52:23 +0700

    Add UpdateJenisViewModel for managing updates to property type

--- app/src/main/java/com/example/bisnisproperti/ui/viewmodel/jenis/UpdateJenisViewModel.kt
+++ app/src/main/java/com/example/bisnisproperti/ui/viewmodel/jenis/UpdateJenisViewModel.kt
@@ -1,2 +1,56 @@
 package com.example.bisnisproperti.ui.viewmodel.jenis
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.bisnisproperti.model.JenisProperti
+import com.example.bisnisproperti.repository.JenisPropertiRepository
+import kotlinx.coroutines.launch
+
+class UpdateJenisViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val jenisPropertiRepository: JenisPropertiRepository
+) : ViewModel() {
+
+    var uiState by mutableStateOf(InsertJenisUiState())
+        private set
+
+    private val idJenis: Int = checkNotNull(savedStateHandle["id_jenis"])
+
+    init {
+        viewModelScope.launch {
+            try {
+                val jenisProperti = jenisPropertiRepository.getJenisPropertiById(idJenis.toString())
+                uiState = jenisProperti.toInsertJenisUiState()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    fun updateJenis() {
+        viewModelScope.launch {
+            try {
+                // Konversi UI state menjadi objek JenisProperti
+                val jenisProperti = uiState.insertJenisUiEvent.toJenisProperti()
+                jenisPropertiRepository.updateJenisProperti(
+                    idJenis.toString(),
+                    jenisProperti
+                )
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    fun updateState(insertJenisUiEvent: InsertJenisUiEvent) {
+        uiState = uiState.copy(insertJenisUiEvent = insertJenisUiEvent)
+    }
+}
+
+fun JenisProperti.toInsertJenisUiState(): InsertJenisUiState = InsertJenisUiState(
+    insertJenisUiEvent = this.toInsertJenisUiEvent()
+)
