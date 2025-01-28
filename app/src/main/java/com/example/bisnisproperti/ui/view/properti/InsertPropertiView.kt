4e049d0d5be3990a56da37730d2cb967a6726278
Ryvqi <rifqikhabibi03@gmail.com>
2025-01-28 11:00:10 +0700

    menambahkan fungsi InsertPropertiView

--- app/src/main/java/com/example/bisnisproperti/ui/view/properti/InsertPropertiView.kt
+++ app/src/main/java/com/example/bisnisproperti/ui/view/properti/InsertPropertiView.kt
@@ -4,16 +4,63 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.bisnisproperti.ui.viewmodel.properti.InsertPropertiUiEvent
 import com.example.bisnisproperti.ui.viewmodel.properti.InsertPropertiUiState
+import com.example.bisnisproperti.ui.viewmodel.properti.InsertPropertiViewModel
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun InsertPropertiView(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InsertPropertiViewModel = viewModel()
+) {
+    val coroutineScope = rememberCoroutineScope()
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Tambah Properti") },
+                navigationIcon = {
+                    Button(onClick = navigateBack) {
+                        Text("Back")
+                    }
+                }
+            )
+        },
+    ) { innerPadding ->
+        InsertPropertiBody(
+            uiState = viewModel.uiState,
+            onPropertiValueChange = viewModel::updateInsertPropertiState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.insertProperti()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
+}
 
 @Composable
 fun InsertPropertiBody(
