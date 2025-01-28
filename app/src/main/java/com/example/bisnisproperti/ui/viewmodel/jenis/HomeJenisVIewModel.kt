16d63e121102a343c62bd2c9d4e7d1db414a4bb1
Ryvqi <rifqikhabibi03@gmail.com>
2025-01-28 16:36:22 +0700

    Add ViewModel for managing JenisProperti data

--- app/src/main/java/com/example/bisnisproperti/ui/viewmodel/jenis/HomeJenisVIewModel.kt
+++ app/src/main/java/com/example/bisnisproperti/ui/viewmodel/jenis/HomeJenisVIewModel.kt
@@ -1,2 +1,56 @@
 package com.example.bisnisproperti.ui.viewmodel.jenis
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.bisnisproperti.model.JenisProperti
+import com.example.bisnisproperti.repository.JenisPropertiRepository
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
+
+sealed class HomeJenisUiState {
+    data class Success(
+        val jenisProperti: List<JenisProperti>
+    ) : HomeJenisUiState()
+
+    object Error : HomeJenisUiState()
+    object Loading : HomeJenisUiState()
+}
+
+class HomeJenisViewModel(private val jenisPropertiRepository: JenisPropertiRepository) : ViewModel() {
+    var jenisPropertiUiState: HomeJenisUiState by mutableStateOf(HomeJenisUiState.Loading)
+        private set
+
+    init {
+        getAllJenisProperti()
+    }
+
+    fun getAllJenisProperti() {
+        viewModelScope.launch {
+            jenisPropertiUiState = HomeJenisUiState.Loading
+            jenisPropertiUiState = try {
+                HomeJenisUiState.Success(jenisPropertiRepository.getAllJenisProperti().data)
+            } catch (e: IOException) {
+                HomeJenisUiState.Error
+            } catch (e: HttpException) {
+                HomeJenisUiState.Error
+            }
+        }
+    }
+
+    fun deleteJenisProperti(idJenis: Int) {
+        viewModelScope.launch {
+            try {
+                jenisPropertiRepository.deleteJenisProperti(idJenis.toString())
+                getAllJenisProperti() // Refresh list after deletion
+            } catch (e: IOException) {
+                jenisPropertiUiState = HomeJenisUiState.Error
+            } catch (e: HttpException) {
+                jenisPropertiUiState = HomeJenisUiState.Error
+            }
+        }
+    }
+}
