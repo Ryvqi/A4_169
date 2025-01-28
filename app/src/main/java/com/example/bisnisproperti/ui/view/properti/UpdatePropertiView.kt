48dc3791e87e25d68807dc697b6ea22928ede3b8
Ryvqi <rifqikhabibi03@gmail.com>
2025-01-28 12:23:45 +0700

    menambahkan fungsi UpdatePropertiBody

--- app/src/main/java/com/example/bisnisproperti/ui/view/properti/UpdatePropertiView.kt
+++ app/src/main/java/com/example/bisnisproperti/ui/view/properti/UpdatePropertiView.kt
@@ -1,2 +1,40 @@
 package com.example.bisnisproperti.ui.view.properti
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.bisnisproperti.ui.viewmodel.properti.InsertPropertiUiEvent
+import com.example.bisnisproperti.ui.viewmodel.properti.InsertPropertiUiState
+
+@Composable
+fun UpdatePropertiBody(
+    uiState: InsertPropertiUiState,
+    onPropertiValueChange: (InsertPropertiUiEvent) -> Unit,
+    onUpdateClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(18.dp),
+        modifier = modifier.padding(12.dp)
+    ) {
+        FormInputProperti(
+            insertUiEvent = uiState.insertPropertiUiEvent,
+            onValueChange = onPropertiValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onUpdateClick,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Update Properti")
+        }
+    }
+}
